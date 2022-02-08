581d02e4b7ce5b610b18f5e3ad9af5c9ac349ade
sonwonjae <sonwj091552@gmail.com>
2022-02-09 01:13:59 +0900

    feat: Init Tag Component skeleton - Issue #6
    - create TagList Compound Component
    - create TagContainer styled-component

--- custom-component/src/components/Tag/Tag.jsx
+++ custom-component/src/components/Tag/Tag.jsx
@@ -1,6 +1,18 @@
 import { memo } from 'react';
-
+import { TagContainer } from './Tag.styled';
 function Tag() {
-  return <></>;
+  return (
+    <TagContainer>
+      <Tag.TagList>
+        {[<span>1</span>, <span>2</span>, <span>3</span>]}
+      </Tag.TagList>
+      <input placeholder="Press enter to add tags" />
+    </TagContainer>
+  );
 }
+
+Tag.TagList = function TagList({ children }) {
+  return <>{children}</>;
+};
+
 export default memo(Tag);
---
+++ custom-component/src/components/Tag/Tag.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const TagContainer = styled.div`
+  display: inline-block;
+  border: 1px solid black;
+  padding: 0.2rem;
+  line-height: 1;
+`;
