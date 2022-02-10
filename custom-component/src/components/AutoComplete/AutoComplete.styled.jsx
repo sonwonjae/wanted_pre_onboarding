210fc6de8775d95ee174e13b43191fc8ed99cfd6
sonwonjae <sonwj091552@gmail.com>
2022-02-10 14:43:56 +0900

    feat: Create AutoComplete Component - Issue #8
    - use Hook of useToggle
    - use Hook of useAutoComplete
    
    - updataeMathWord is autocomplete list update
    - setAutoCompleteWord is display autocomplete list & add word in
      autocomplete list
    - onBlurToAutoComplete use useEffect
    - makeMatchWords is return MatchWord Component list with match word

---
+++ custom-component/src/components/AutoComplete/AutoComplete.styled.jsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { getHexaColor } from 'styles/color/getHexaColor';
+
+export const AutoCompleteContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 500px;
+  margin: 0 auto;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  border: 1px solid ${getHexaColor('gray', 100)};
+  border-radius: 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  padding: 0.5rem;
+  outline: none;
+
+  &:focus {
+    border-radius: ${({ isFocus }) =>
+      isFocus ? '0.75rem 0.75rem 0 0' : '0.75rem'};
+    box-shadow: -4px 5px 8px 0px ${getHexaColor('gray', 300)}5;
+
+    & ~ ul {
+      /* visibility: visible; */
+      box-sizing: border-box;
+      border: 1px solid ${getHexaColor('gray', 100)};
+      border-top: none;
+      border-radius: 0 0 0.75rem 0.75rem;
+    }
+  }
+
+  /* & ~ ul {
+    visibility: hidden;
+  } */
+`;
+
+export const MatchWordList = styled.ul`
+  margin: 0;
+  padding: 0.5rem 0;
+  background-color: white;
+  border-radius: 0 0 0.75rem 0.75rem;
+  box-shadow: -4px 5px 8px 0px ${getHexaColor('gray', 300)}5;
+  position: absolute;
+  width: 100%;
+  top: 100%;
+  list-style: none;
+
+  & ~ input {
+    outline: none;
+    border-radius: 0.75rem 0.75rem 0 0;
+  }
+`;
+
+export const MatchWord = styled.li`
+  cursor: pointer;
+  line-height: 1.5;
+  font-size: 1rem;
+
+  &:hover {
+    background-color: ${getHexaColor('gray', 300)}3;
+  }
+
+  & ~ & {
+    margin-top: 0.25rem;
+  }
+`;
