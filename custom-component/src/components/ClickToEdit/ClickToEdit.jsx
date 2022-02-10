a3be5c247e419b7a219103a804798d1d55bd8887
sonwonjae <sonwj091552@gmail.com>
2022-02-10 14:53:34 +0900

    feat: Init ClickToEdit Component - Issue #10
    - init ClickToEdit Component
    - init ClickToEdit styled-component

---
+++ custom-component/src/components/ClickToEdit/ClickToEdit.jsx
@@ -0,0 +1,7 @@
+import { memo } from 'react';
+
+function ClickToEdit() {
+  return <></>;
+}
+
+export default memo(ClickToEdit);
--- custom-component/src/components/index.js
+++ custom-component/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Toggle } from './Toggle/Toggle';
 export { default as Modal } from './Modal/Modal';
 export { default as Tag } from './Tag/Tag';
 export { default as AutoComplete } from './AutoComplete/AutoComplete';
+export { default as ClickToEdit } from './ClickToEdit/ClickToEdit';
