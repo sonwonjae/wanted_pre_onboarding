b57cd012091cbbb00aa5d41d359e49cf90d6cb3d
sonwonjae <sonwj091552@gmail.com>
2022-02-11 13:12:28 +0900

    feat: Create util makeRouteComponents - Issue #12
    - wrapSuspense: return wrap Suspense Compnent
    - makeRouteComponents: return Route in wrapSuspense & path with
      lazyComponent property key

---
+++ custom-component/src/utils/Routes/makeRouteComponents.js
@@ -0,0 +1,21 @@
+import { Suspense } from 'react';
+import { Route } from 'react-router-dom';
+
+const wrapSuspense = (Component) => (
+  <Suspense fallback={<>...Loading</>}>
+    <Component />
+  </Suspense>
+);
+
+export const makeRouteComponents = (lazyComponents) =>
+  Object.keys(lazyComponents).map((tab) => {
+    const path = tab === 'Index' ? '/' : tab === 'PageNotFound' ? '*' : tab;
+
+    return (
+      <Route
+        key={tab}
+        path={path}
+        element={wrapSuspense(lazyComponents[tab])}
+      />
+    );
+  });
