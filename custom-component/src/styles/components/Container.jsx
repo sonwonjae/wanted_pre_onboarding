70c949ece52ac2f73401b97a5dc81abbbf53ed90
sonwonjae <sonwj091552@gmail.com>
2022-02-11 17:08:33 +0900

    feat: Edit Toggle styled-component - Issue #2
    - add ToggleContainer
    - delete src/styles/components

--- custom-component/src/styles/components/Container.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: ${({ gap }) => gap || 10}px;
-`;
