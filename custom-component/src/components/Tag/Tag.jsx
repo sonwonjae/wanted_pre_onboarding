b21b903e0541db7e2e3f86f3abbe1d2dab218913
sonwonjae <sonwj091552@gmail.com>
2022-02-11 17:02:33 +0900

    feat: Structure Tag Component - Issue #14
    - Compound
    - style
    - view

--- custom-component/src/components/Tag/Tag.jsx
+++ custom-component/src/components/Tag/Tag.jsx
@@ -1,40 +1,19 @@
-import { memo, useCallback } from 'react';
-import { TagContainer, TagItemWrapper, Input } from './Tag.styled';
+import { memo } from 'react';
+import { TagContainer, Input } from './style';
 import { useTagControl } from 'hooks';
+import { TagList } from './Compound';
+import { useTagView } from './view';
 
 function Tag() {
   const { tags, addTagItem, deleteTagItem } = useTagControl();
-
-  const makeTagList = useCallback(
-    (tags) => {
-      return [...tags.values()].map((tag) => (
-        <Tag.TagItem key={tag} onClick={deleteTagItem(tag)}>
-          {tag}
-        </Tag.TagItem>
-      ));
-    },
-    [tags]
-  );
+  const { makeTagList } = useTagView({ deleteTagItem });
 
   return (
     <TagContainer>
-      <Tag.TagList>{makeTagList(tags)}</Tag.TagList>
+      <TagList>{makeTagList(tags)}</TagList>
       <Input onKeyUp={addTagItem} placeholder="Press enter to add tags" />
     </TagContainer>
   );
 }
-
-Tag.TagList = function TagList({ children }) {
-  return <>{children}</>;
-};
-
-Tag.TagItem = function TagItem({ children, onClick }) {
-  return (
-    <TagItemWrapper>
-      {children}
-      <button onClick={onClick}>×</button>
-    </TagItemWrapper>
-  );
-};
 
 export default memo(Tag);
