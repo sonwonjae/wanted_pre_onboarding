ce05118ba26d92d987f57399b2fc8599c0bab248
sonwonjae <sonwj091552@gmail.com>
2022-02-11 16:50:31 +0900

    feat: Structure Modal Component - Issue #14
    - Compound
    - style

--- custom-component/src/components/Modal/Modal.jsx
+++ custom-component/src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
-import { useModalToggle, useFocusableElements } from 'hooks';
-import { memo, useCallback, useRef } from 'react';
-import { createPortal } from 'react-dom';
-import { Button, Overlay, DialogContainer } from './Modal.styled';
+import { useModalToggle } from 'hooks';
+import { memo } from 'react';
+import { Button } from './style';
+import { Dialog } from './Compound';
 
 function Modal() {
   const [showModal, toggleModal] = useModalToggle(false);
@@ -9,46 +9,9 @@ function Modal() {
   return (
     <>
       <Button onClick={toggleModal}>Open Modal</Button>
-      {showModal && <Modal.Dialog closeModal={toggleModal} />}
+      {showModal && <Dialog closeModal={toggleModal} />}
     </>
   );
 }
-
-Modal.Dialog = function Dialog({ closeModal }) {
-  const modalRef = useRef();
-  const [firstEl, lastEl] = useFocusableElements(modalRef);
-
-  const keyboardTrapInModal = useCallback(
-    (e) => {
-      if (e.key === 'Escape') return closeModal();
-      if (e.key !== 'Tab') return;
-
-      const trapEl = e.shiftKey ? firstEl : lastEl;
-      const focusEl = e.shiftKey ? lastEl : firstEl;
-
-      if (document.activeElement === trapEl) {
-        focusEl.focus();
-        e.preventDefault();
-      }
-    },
-    [firstEl, lastEl]
-  );
-
-  const focusOnFirstEl = useCallback(() => firstEl.focus(), [firstEl]);
-
-  return createPortal(
-    <Overlay onClick={closeModal}>
-      <DialogContainer
-        ref={modalRef}
-        onKeyDown={keyboardTrapInModal}
-        onClick={focusOnFirstEl}
-      >
-        <button onClick={closeModal}>×</button>
-        HELLO CODESTATES!
-      </DialogContainer>
-    </Overlay>,
-    document.body
-  );
-};
 
 export default memo(Modal);
